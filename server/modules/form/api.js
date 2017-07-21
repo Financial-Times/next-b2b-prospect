2de63824f5d2bf39ae20e57bd8849e65623060a3
Alex Naish <alex@naish.io>
2017-07-21 09:35:28 +0100

    fix pa11y errors  🐿2.5.11

--- server/modules/form/api.js
+++ server/modules/form/api.js
@@ -1,12 +1,14 @@
 export default {
 	form: (req, res, next) => {
 		res.render('form', {
+			title: 'Signup',
 			layout: 'vanilla',
 			countryCode: res.locals.countryCode
 		});
 	},
 	confirmation: (req, res, next) => {
 		res.render('confirm', {
+			title: 'Signup',
 			layout: 'vanilla'
 		});
 	},
