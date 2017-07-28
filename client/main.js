bb3ba1baf89ae6912e9741179eb687685eadcb46
Alex Naish <alex@naish.io>
2017-07-28 16:33:06 +0100

    show overlay and add postMessage for formHeight  🐿 v2.5.13

--- client/main.js
+++ client/main.js
@@ -1,6 +1,23 @@
 const OForms = require('o-forms');
+const overlay = document.querySelector('.prospect-form__overlay');
 const formsEl = document.querySelector('[data-o-component="o-forms"]');
 
 if (formsEl) {
-	new OForms(formsEl)
+	new OForms(formsEl);
+
+	var observer = new MutationObserver(mutations => {
+		mutations.forEach(mutation => {
+			if(mutation.target.className.includes('error')) {
+				parent.postMessage(formsEl.clientHeight, 'ft.com');
+			}
+		});
+	});
+
+	var config = { subtree: true, attributes: true };
+	observer.observe(formsEl, config);
+
+	formsEl.addEventListener("submit", () => {
+		overlay.classList.add('prospect-form__overlay--active');
+	});
+
 }
