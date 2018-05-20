f21841197192f6bdc90f28fb49bef60bf190f05f
doramatadora <dora.militaru@ft.com>
2018-05-20 23:34:34 +0100

    update Marketo schema  🐿 v2.8.0

--- server/modules/marketo/constants.js
+++ server/modules/marketo/constants.js
@@ -4,22 +4,45 @@ export const NOT_FOUND_ERROR = 'marketo/not_found';
 export const UNEXPECTED_RESULT_ERROR = 'marketo/unexpected_result';
 export const LEAD_ALREADY_EXISTS_ERROR = 'lead_already_exists';
 
-export const SCHEMA = Joi.object().keys({
-    firstName: Joi.string().required(),
-    lastName: Joi.string().required(),
-    title: Joi.string().required(),
-    company: Joi.string().required(),
-    email: Joi.string().email().required(),
-    phone: Joi.string().min(5).max(15).regex(/^([0-9()+\- ])+$/, 'all numbers').required(),
-    country: Joi.string().required(),
-    numberOfEmployees: Joi.number().default('0'),
-    rating: Joi.string().required(),
-    Third_Party_Opt_In__c: Joi.bool().required(),
-    leadSource: Joi.string().empty('').default('FT.com'),
-    Industry_Sector__c: Joi.string().empty('').default('null'),
-    Lead_Type__c: Joi.string().empty('').default('Corporate'),
-    CPCCampaign__c: Joi.string().empty('').default(''),
-    GCLID__c: Joi.string().empty('').default(''),
-    Comments: Joi.string().empty('').default(''),
-    Job_Function__c: Joi.string().empty('').default('')
-}).rename('jobTitle', 'title', { ignoreUndefined: true });
+export const SCHEMA = Joi.object()
+	.keys({
+		firstName: Joi.string().required(),
+		lastName: Joi.string().required(),
+		title: Joi.string().required(),
+		company: Joi.string().required(),
+		email: Joi.string()
+			.email()
+			.required(),
+		phone: Joi.string()
+			.min(5)
+			.max(15)
+			.regex(/^([0-9()+\- ])+$/, 'all numbers')
+			.required(),
+		country: Joi.string().required(),
+		numberOfEmployees: Joi.number().default('0'),
+		rating: Joi.string().required(),
+		Third_Party_Opt_In__c: Joi.bool().optional(),
+		leadSource: Joi.string()
+			.empty('')
+			.default('FT.com'),
+		Industry_Sector__c: Joi.string()
+			.empty('')
+			.default('null'),
+		Lead_Type__c: Joi.string()
+			.empty('')
+			.default('Corporate'),
+		CPCCampaign__c: Joi.string()
+			.empty('')
+			.default(''),
+		GCLID__c: Joi.string()
+			.empty('')
+			.default(''),
+		Comments: Joi.string()
+			.empty('')
+			.default(''),
+		Job_Function__c: Joi.string()
+			.empty('')
+			.default('')
+	})
+	.rename('jobTitle', 'title', { ignoreUndefined: true })
+	.pattern(/Consent_\w*__\w*/, Joi.boolean());
