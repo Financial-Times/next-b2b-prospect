707c3acf780aa183c6dfdd0f0a2612c54f9a9d07
Jennifer Kerr <jennifer.kerr@ft.com>
2018-07-31 11:43:48 +0100

    add email field to successfully submit to marketo (#65)
     🐿 v2.10.0
    ## Feature Description
    Update the unmasking mid-step to add in email field, which is needed to submit to marketo. If a session exists the email address is extracted and pre-populates the email field
    
    ~**FYI**
    This depends on `n-conversion-forms` which may be reverted (https://github.com/Financial-Times/next-signup/pull/1568) due to direct debit payment issue, so I'll wait for that to be resolved before merging~
    
    ## Link to Ticket / Card:
    https://trello.com/c/wdFghfoE
    
    ## Screenshots:
    ![image](https://user-images.githubusercontent.com/17846996/43259754-3df474f0-90cf-11e8-89ad-7459cc2d6deb.png)

---
+++ server/modules/graph-ql/getUserEmail.js
@@ -0,0 +1,27 @@
+import { metrics } from '@financial-times/n-express';
+import membQl from '@financial-times/n-memb-gql-client';
+import STATIC_DATA from 'n-common-static-data';
+import _get from 'lodash/get';
+import MaskLogger from '@financial-times/n-mask-logger';
+const logger = new MaskLogger(['email', 'password']);
+const MEMB_GRAPH_QL_SVC = 'membership-graph-ql';
+
+module.exports = async function returnEmail (session) {
+    try {
+        const membQueryResult = await membQl.query(STATIC_DATA.graphQL.userEmailQuery, { session });
+        if (!membQueryResult._ok) {
+            throw new Error ({name: 'content_error', message:'Membership API response content error'});
+        } else {
+            metrics.count(`${MEMB_GRAPH_QL_SVC}.res.success`, 1);
+        }
+        return _get(membQueryResult, 'data.userBySession.profiles.basic.email');
+    } catch (e) {
+        if (e.name === 'content_error') {
+            metrics.count(`${MEMB_GRAPH_QL_SVC}.res.error`, 1);
+            logger.warn({ svc: MEMB_GRAPH_QL_SVC, operation: 'query', name: 'getUserEmail', result: 'error' }, { message: _get(membQueryResult, 'errors[0].message') });
+        } else {
+            metrics.count(`${MEMB_GRAPH_QL_SVC}.res.failure`, 1);
+            logger.error({ svc: MEMB_GRAPH_QL_SVC, operation: 'query', name: 'getUserEmail', result: 'failure' }, e);
+        }
+    }
+};
